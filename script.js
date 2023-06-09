0bff01bc3e3aa168dfbb01033503302dc1c5cff2
vinay kumar <vkumar8192449@gmail.com>
2023-06-10 00:57:20 +0530

    day2

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const answer = Math.ceil(Math.random() * 19);
-var score = 10;
+var score = 100;
+var deduct = 10;
 document.getElementsByClassName('highscore')[0].innerText = (localStorage.Highscore) ? localStorage.Highscore : 0;
 
 function check() {
     let num = document.getElementsByClassName('guess')[0].value;
     if (num) {
-        if (score == 0) {
+        score -= deduct;
+        if ((score == 0 && deduct == 10) || (score == -5 && deduct == 15) || (score == 0 && deduct == 20)) {
             lost();
+            score = 0;
         }
         else if (num < answer) {
             document.getElementsByClassName('message')[0].innerText = "Low!!!";
-            score--;
         }
         else if (num > answer) {
             document.getElementsByClassName('message')[0].innerText = "High!!!";
-            score--;
         }
         else {
             document.getElementsByClassName('message')[0].innerText = "Correct!!!";
@@ -44,4 +45,44 @@ function lost() {
 }
 function restart() {
     location.reload();
+}
+function openmodal() {
+    document.getElementById('backmodal').style.display = "block";
+    document.getElementById('modal').style.display = "block";
+}
+
+window.addEventListener("click", function (e) {
+    removemodel(e)
+});
+function removemodel(e) {
+    e.target == document.getElementById('backmodal') ? document.getElementById('backmodal').style.display = "none" : false;
+}
+function changemode(e) {
+    var esy = document.getElementsByClassName('easy')[0];
+    var med = document.getElementsByClassName('med')[0];
+    var hrd = document.getElementsByClassName('hrd')[0];
+    if (e == 'EASY') {
+        deduct = 10;
+        esy.style.backgroundColor = "#9fe875";
+        med.style.backgroundColor = "#cbc8c8";
+        hrd.style.backgroundColor = "#cbc8c8";
+    }
+    else if (e == 'MEDIUM') {
+        deduct = 15;
+        esy.style.backgroundColor = "#cbc8c8";
+        med.style.backgroundColor = "#f9c766";
+        hrd.style.backgroundColor = "#cbc8c8";
+    }
+    else {
+        deduct = 20;
+        esy.style.backgroundColor = "#cbc8c8";
+        med.style.backgroundColor = "#cbc8c8";
+        hrd.style.backgroundColor = "#f47f6b";
+    }
+    score = 100;
+    document.getElementsByClassName('score')[0].innerText = score;
+    setTimeout(() => {
+        document.getElementById('backmodal').style.display = "none";
+    }, 200);
+    document.getElementsByClassName('between')[0].innerText = `MODE: ${e}`;
 }
